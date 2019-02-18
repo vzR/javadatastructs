c6fa818f3dc403eddb50a7d09e4af19bbd70e6e9
hansensoulie <vezcri@yahoo.com>
2019-02-18 19:29:04 +0100

    list interface

--- src/ListIterator/List.java
+++ src/ListIterator/List.java
@@ -1,4 +1,13 @@
 package ListIterator;
 
-public class List {
+public interface List<E> {
+    // returns the number of elements in list
+    int size();
+
+    // returns whether the list is empty
+    boolean isEmpty();
+
+    // returns (but does not remove the element at index i
+    E get(int i) throws IndexOutOfBoundsException;
+
 }
